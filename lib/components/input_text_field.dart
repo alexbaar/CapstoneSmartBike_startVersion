47f8da7ba181baa703626afc29a2d022692363d8
AlexBaar <63990224+alexbaar@users.noreply.github.com>
2024-03-29 13:59:33 +1100

    Update input_text_field.dart
    Added show/hide password visibility and hintText

--- lib/components/input_text_field.dart
+++ lib/components/input_text_field.dart
@@ -1,45 +1,77 @@
 import 'package:flutter/material.dart';
 import 'package:phone_app/utilities/constants.dart';
 
-class InputTextField extends StatelessWidget {
+class InputTextField extends StatefulWidget {
   // custom constructor
-  InputTextField({required this.buttonText, this.fieldController, this.height});
-
-  // use VoidCallback instead of Function
+  InputTextField({
+    required this.buttonText,
+    this.fieldController,
+    this.height,
+    this.enableToggle = false, // Add a new property to enable/disable toggle
+  }) : obscureText = enableToggle
+            ? true
+            : false; // Initialize obscureText based on enableToggle
 
   final String buttonText;
-  final TextEditingController? fieldController; // optional
-  final double? height; // optional
+  final TextEditingController? fieldController;
+  final double? height;
+  bool obscureText;
+  final bool enableToggle; // New property to indicate toggle enablement
+
+  @override
+  _InputTextFieldState createState() => _InputTextFieldState();
+}
+
+class _InputTextFieldState extends State<InputTextField> {
+  void toggleObscured() {
+    setState(() {
+      widget.obscureText = !widget.obscureText;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: height ?? 60,
+      height: widget.height ?? 60,
       decoration: BoxDecoration(
         border: Border.all(
-          color: kLoginRegisterBtnColour, // Default border color
-          width: 2.0, // Default border width
+          color: kLoginRegisterBtnColour,
+          width: 2.0,
         ),
-        color:
-            kFillInText.withOpacity(0.1), // Set the fill color and opacity here
-        borderRadius:
-            BorderRadius.circular(10.0), // Set border radius if needed
+        color: kFillInText.withOpacity(0.1),
+        borderRadius: BorderRadius.circular(10.0),
       ),
       child: TextFormField(
         textAlign: TextAlign.left,
-        controller: fieldController,
-        obscureText: true,
+        controller: widget.fieldController,
+        obscureText: widget.obscureText,
         decoration: InputDecoration(
-          labelText: buttonText,
-          labelStyle: TextStyle(color: kFillInText),
+          hintText: widget.buttonText,
+          hintStyle: kSubSubTitlePurple,
           border: UnderlineInputBorder(
-            borderSide: BorderSide.none, // No underline
+            borderSide: BorderSide.none,
           ),
-          contentPadding: EdgeInsets.fromLTRB(20, 30, 0, 0),
+          contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+          suffixIcon: widget.enableToggle
+              ? _buildSuffixIcon()
+              : null, // Conditionally show suffix icon
         ),
+      ),
+    );
+  }
 
-        //style: TextStyle(fontSize: height != null ? height! * 0.5 : 22.0),
+  Widget _buildSuffixIcon() {
+    return GestureDetector(
+      onTap: toggleObscured,
+      child: Icon(
+        widget.obscureText
+            ? Icons.visibility_rounded
+            : Icons.visibility_off_rounded,
+        size: 24,
+        color: kLoginRegisterBtnColour,
       ),
     );
   }
 }
+
+
